3bc6a64212949d92b60440b4a484cfd11a80dd8b
Marta Tavera <martatavera@gmail.com>
2025-04-04 16:43:25 +0100

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import './index.css'
-import Portfolio from './components/home/home.tsx'
 import Navbar from './components/Navbar/Navbar.tsx'
 import Contact from './components/contact/Contact.tsx';
 import About from './components/about/About.tsx';
