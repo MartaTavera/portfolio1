fb8e02dda23c3235edc4fcdfed730a3ac9aba30e
Marta Tavera <martatavera@gmail.com>
2025-04-28 14:40:04 +0100

    make navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,12 +5,14 @@ import './Navbar.css';
 const Navbar = () => {
     return (
       <nav className="navbar">
-        
-        <a href="/portfolio1">Home</a>
-        <a href="#tools">Tools</a>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
+        <a id="pageTitle" href="/portfolio1">Marta Tavera</a>
+        <div className="nav-links">
+          <a href="/portfolio1">Home</a>
+          <a href="#tools">Tools</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
       </nav>
     );
   };
